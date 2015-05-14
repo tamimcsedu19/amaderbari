ea8c311b4558777a78ecf1b6d8e08efc918b8aba
Your Name <jony.du.12.12.12@gmail.com>
2015-05-15 00:41:37 +0600

    import solved

--- new_import_from_here/amaderbari/src/main/java/sdp/ratajo/amaderbari/dao/FlatSearcher.java
+++ new_import_from_here/amaderbari/src/main/java/sdp/ratajo/amaderbari/dao/FlatSearcher.java
@@ -1,7 +1,7 @@
 package sdp.ratajo.amaderbari.dao;
 import java.util.List;
-
-import sdp.ratajo.amaderbari.dto.*;
+import sdp.ratajo.amaderbari.dto.addresspack.Address;
+import sdp.ratajo.amaderbari.dto.flatpack.Flat;
 public interface FlatSearcher {
 
 	List<Flat> search(Address addr);
