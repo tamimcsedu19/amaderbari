97d5242140b0a4cad35562c304bc077fec29ef56
tamimcsedu19 <tamim.tamim1382@gmail.com>
2015-05-15 18:58:19 +0600

    added mysql

--- new_import_from_here/amaderbari/src/main/java/sdp/ratajo/amaderbari/config/MvcConfiguration.java
+++ new_import_from_here/amaderbari/src/main/java/sdp/ratajo/amaderbari/config/MvcConfiguration.java
@@ -36,7 +36,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/ratajo");
+        dataSource.setUrl("jdbc:mysql://182.48.75.69:3306/ratajo");
         dataSource.setUsername("ratajo");
         dataSource.setPassword("ratajo");
          
